c90157b29333e4a980538977b834986694df36e4
Ilya Sapunov <ilya.v.sapunov@gmail.com>
2022-08-12 15:02:40 +0300

    first commit

---
+++ tap_youtravelapi/streams.py
@@ -0,0 +1,75 @@
+"""Stream type classes for tap-youtravelapi."""
+
+from pathlib import Path
+from typing import Any, Dict, Optional, Union, List, Iterable
+
+from singer_sdk import typing as th  # JSON Schema typing helpers
+
+from tap_youtravelapi.client import YoutravelApiStream
+
+# TODO: Delete this is if not using json files for schema definition
+SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
+# TODO: - Override `UsersStream` and `GroupsStream` with your own stream definition.
+#       - Copy-paste as many times as needed to create multiple stream types.
+
+
+class ToursStream(YoutravelApiStream):
+    """Define custom stream."""
+    name = "tours"
+    path = "/serp/tours/?currency=rub&lang=en&take=10"
+    primary_keys = ["id"]
+    replication_key = None
+
+    schema = th.PropertiesList(
+        th.Property("id", th.IntegerType),
+        th.Property("title", th.StringType),
+        th.Property("continents", th.ArrayType(
+            th.StringType
+        )),
+        th.Property("countries", th.ArrayType(
+            th.StringType
+        )),
+        th.Property("highlights", th.StringType),
+        th.Property("regions", th.ArrayType(
+            th.StringType
+        )),
+        th.Property("locations", th.ArrayType(
+            th.StringType
+        )),
+        th.Property("types", th.ArrayType(
+            th.ObjectType(
+                th.Property("main", th.StringType),
+                th.Property("title", th.StringType),
+            )
+        )),
+        th.Property("expert", th.ObjectType(
+            th.Property("id", th.IntegerType),
+            th.Property("rating", th.IntegerType),
+            th.Property("count_reviews", th.IntegerType),
+            th.Property("avatar", th.StringType),
+            th.Property("name", th.StringType),
+        )),
+        th.Property("dates", th.ObjectType(
+            th.Property("total", th.IntegerType),
+            th.Property("group", th.ObjectType(
+                th.Property("id", th.IntegerType),
+                th.Property("date_from", th.IntegerType),
+                th.Property("date_to", th.IntegerType),
+                th.Property("guarantee", th.BooleanType),
+                th.Property("price", th.NumberType),
+                th.Property("prices_with_discount", th.NumberType),
+                th.Property("discount_percent", th.NumberType),
+                th.Property("free_spaces", th.IntegerType),
+            ))
+        )),
+        th.Property("languages", th.ArrayType(
+            th.StringType
+        )),
+        th.Property("is_new", th.BooleanType),
+        th.Property("link", th.StringType),
+        th.Property("cancellation", th.BooleanType),
+        th.Property("use_custom_refund_policy", th.BooleanType),
+        th.Property("is_private", th.BooleanType),
+        th.Property("cashback_mir", th.BooleanType),
+        th.Property("is_latest", th.BooleanType),
+    ).to_dict()
---
+++ tap_youtravelapi/tap.py
@@ -0,0 +1,33 @@
+"""YoutravelApi tap class."""
+
+from typing import List
+
+from singer_sdk import Tap, Stream
+from singer_sdk import typing as th  # JSON schema typing helpers
+from tap_youtravelapi.streams import (
+    YoutravelApiStream,
+    ToursStream,
+)
+# TODO: Compile a list of custom stream types here
+#       OR rewrite discover_streams() below with your custom logic.
+STREAM_TYPES = [
+    ToursStream,
+]
+
+
+class TapYoutravelApi(Tap):
+    """YoutravelApi tap class."""
+    name = "tap-youtravelapi"
+
+    # TODO: Update this section with the actual config values you expect:
+    config_jsonschema = th.PropertiesList(
+        th.Property(
+            "start_date",
+            th.DateTimeType,
+            description="The earliest record date to sync"
+        ),
+    ).to_dict()
+
+    def discover_streams(self) -> List[Stream]:
+        """Return a list of discovered streams."""
+        return [stream_class(tap=self) for stream_class in STREAM_TYPES]
